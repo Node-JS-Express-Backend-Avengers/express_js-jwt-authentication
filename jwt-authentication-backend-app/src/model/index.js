00ef33a040ac4f0a829c20cbcc1180f26101f114
quantum-adrenaline-1 <yogiraj.chitam@gmail.com>
2023-10-23 03:28:44 +0530

    Updated the name of MongoDB collection to be 'users' for persisting the 'User' instances in DB

--- jwt-authentication-backend-app/src/model/index.js
+++ jwt-authentication-backend-app/src/model/index.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 
 const userSchemaObj = require('./schema/user');
 
-const userModel = mongoose.model('user', userSchemaObj, 'user');
+const userModel = mongoose.model('user', userSchemaObj, 'users');
 
 module.exports = userModel;
