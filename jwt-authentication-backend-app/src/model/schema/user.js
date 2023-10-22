f2ba127f9d9acc265ea2dd5ea66d8d533f121f51
quantum-adrenaline-1 <yogiraj.chitam@gmail.com>
2023-10-22 17:54:52 +0530

    Defined User ODM schema for MongoDB using Mongoose

---
+++ jwt-authentication-backend-app/src/model/schema/user.js
@@ -0,0 +1,10 @@
+const mongooseObj = require('mongoose');
+
+const schemaObj = mongooseObj.Schema;
+
+const userSchemaObj = new schemaObj({
+    email: String,
+    password: String
+});
+
+module.exports = userSchemaObj;
