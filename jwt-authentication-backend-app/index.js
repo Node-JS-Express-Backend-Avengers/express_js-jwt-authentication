91264ca1ca782b1e380b39e77bffa5ff1eb05f1a
quantum-adrenaline-1 <yogiraj.chitam@gmail.com>
2023-10-22 16:45:29 +0530

    Added documentation for root index.js and added '/api' route for the 'api' router imported in index.js

--- jwt-authentication-backend-app/index.js
+++ jwt-authentication-backend-app/index.js
@@ -2,12 +2,21 @@ const express = require('express');
 
 const constants = require('./src/util/constant');
 
+// express application object
 const expressApplication = express();
 
+// APIs on apiRouter
+const apiRouter = require('./src/api/');
+
+// '/api' route assignment for APIs on 'apiRouter'
+expressApplication.use('/api', apiRouter);
+
+// application health-status check
 expressApplication.get('/', (req, res) => {
     res.send('Application "JWT authentication" is up and running');
 });
 
+// start application on the assigned port
 expressApplication.listen(constants.PORT, (req, res) => {
     console.log(`application is running on port ${constants.PORT}`);
 });
