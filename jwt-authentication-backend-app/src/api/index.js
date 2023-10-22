c10c4afb5fbcea15d40fbafe10636f263f0a97e0
quantum-adrenaline-1 <yogiraj.chitam@gmail.com>
2023-10-23 03:32:53 +0530

    Imported database connection object, imported 'user' model configured using Mongoose and completed 'register' user API to persist 'user' instance in MongoDB successfully

--- jwt-authentication-backend-app/src/api/index.js
+++ jwt-authentication-backend-app/src/api/index.js
@@ -1,8 +1,31 @@
 const express = require('express');
 const router = express.Router();
 
+// database connection object
+const dbConnection = require('../persistence/');
+
+// user model object
+const userModel = require('../model/');
+
+// route info API
 router.get('/', (req, res) => {
     res.send(`Serving the requests over route ${req.baseUrl}`);
+});
+
+// user registration API
+router.post('/register', async (req, res) => {
+    const userPayload = req.body;
+    console.log(`This is payload for '/register': ${JSON.stringify(userPayload)}`);
+    const userObj = new userModel(userPayload);
+    console.log(`This is user object using payload from '/register': ${JSON.stringify(userObj)}`);
+    let persistedUser = await userObj.save();
+    if (persistedUser && persistedUser == userObj) {
+        console.log(`User instance ${userObj} stored in DB successfully`);
+        res.status(201).send({data: persistedUser, message: "User saved successfully!"});
+    } else if (!persistedUser || persistedUser != userObj) {
+        console.log(`Failed to persist the user ${JSON.stringify(userObj)}`);
+        res.status(500).send(null);
+    }
 });
 
 module.exports = router;
