1d7c6475dfe372bd8f2aa9d5aaae5b5d22f1c668
quantum-adrenaline-1 <yogiraj.chitam@gmail.com>
2023-10-22 15:46:04 +0530

    Added GET API for checking the running status of the backend application

--- jwt-authentication-backend-app/index.js
+++ jwt-authentication-backend-app/index.js
@@ -4,6 +4,10 @@ const constants = require('./src/util/constant');
 
 const expressApplication = express();
 
+expressApplication.get('/', (req, res) => {
+    res.send('Application "JWT authentication" is up and running');
+});
+
 expressApplication.listen(constants.PORT, (req, res) => {
     console.log(`application is running on port ${constants.PORT}`);
 });
