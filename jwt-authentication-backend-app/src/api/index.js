e7d0299ae56b35594492e385307447bcee451f8f
quantum-adrenaline-1 <yogiraj.chitam@gmail.com>
2023-10-23 19:54:08 +0530

    Implemented events related APIs for delivering regular and special events and grouped all the APIs in 'api' module inside JS objects

--- jwt-authentication-backend-app/src/api/index.js
+++ jwt-authentication-backend-app/src/api/index.js
@@ -8,57 +8,79 @@ const dbConnection = require("../persistence/");
 const userModel = require("../model/");
 
 // route info API
-router.get("/", (req, res) => {
-  res.send(`Serving the requests over route ${req.baseUrl}`);
-});
+const healthCheckAPI = {
+  healthCheckAPI: router.get("/", (req, res) => {
+    res.send(`Serving the requests over route ${req.baseUrl}`);
+  }),
+};
 
-// user registration API
-router.post("/register", async (req, res) => {
-  const userPayload = req.body;
-  console.log(
-    `This is payload for '/register': ${JSON.stringify(userPayload)}`
-  );
-  const userObj = new userModel(userPayload);
-  console.log(
-    `This is user object using payload from '/register': ${JSON.stringify(
-      userObj
-    )}`
-  );
-  let persistedUser = await userObj.save();
-  if (persistedUser && persistedUser == userObj) {
-    console.log(`User instance ${userObj} stored in DB successfully`);
-    return res
-      .status(201)
-      .send({ data: persistedUser, message: "User saved successfully!" });
-  } else if (!persistedUser || persistedUser != userObj) {
-    console.log(`Failed to persist the user ${JSON.stringify(userObj)}`);
-    return res.status(500).send(null);
-  }
-});
+// authentication API
+const authenticationAPI = {
+  // user registration API
+  register: router.post("/register", async (req, res) => {
+    const userPayload = req.body;
+    console.log(
+      `This is payload for '/register': ${JSON.stringify(userPayload)}`
+    );
+    const userObj = new userModel(userPayload);
+    console.log(
+      `This is user object using payload from '/register': ${JSON.stringify(
+        userObj
+      )}`
+    );
+    let persistedUser = await userObj.save();
+    if (persistedUser && persistedUser == userObj) {
+      console.log(`User instance ${userObj} stored in DB successfully`);
+      return res
+        .status(201)
+        .send({ data: persistedUser, message: "User saved successfully!" });
+    } else if (!persistedUser || persistedUser != userObj) {
+      console.log(`Failed to persist the user ${JSON.stringify(userObj)}`);
+      return res.status(500).send(null);
+    }
+  }),
+  // user login API
+  login: router.post("/login", async (req, res) => {
+    const credentialsPayload = req.body;
+    const fetchedUser = await userModel
+      .findOne({ email: credentialsPayload.email })
+      .exec();
+    console.log(
+      `Login attempt by persistent user: ${JSON.stringify(fetchedUser)}`
+    );
+    if (!fetchedUser) {
+      return res.status(404).send({ message: "User not found!" });
+    } else if (fetchedUser.password != credentialsPayload.password) {
+      return res
+        .status(401)
+        .send({ message: "Invalid email or password, please try again" });
+    }
+    // case: valid email and password for authentication
+    else {
+      return res.status(200).send({
+        data: { email: fetchedUser.email },
+        message: `User '${credentialsPayload.email}' logged in successfully!`,
+      });
+    }
+  }),
+};
 
-// user login API
-router.post("/login", async (req, res) => {
-  const credentialsPayload = req.body;
-  const fetchedUser = await userModel
-    .findOne({ email: credentialsPayload.email })
-    .exec();
-  console.log(
-    `Login attempt by persistent user: ${JSON.stringify(fetchedUser)}`
-  );
-  if (!fetchedUser) {
-    return res.status(404).send({ message: "User not found!" });
-  } else if (fetchedUser.password != credentialsPayload.password) {
-    return res
-      .status(401)
-      .send({ message: "Invalid email or password, please try again" });
-  }
-  // case: valid email and password for authentication
-  else {
-    return res.status(200).send({
-      data: { email: fetchedUser.email },
-      message: `User '${credentialsPayload.email}' logged in successfully!`,
-    });
-  }
-});
+// event fetching API
+const eventAPI = {
+  // public event
+  events: router.get("/events", (req, res) => {
+    const fetchedEvents = require("../static_store/events");
+    if (fetchedEvents) {
+      return res.status(200).send(fetchedEvents);
+    }
+  }),
+  // special event
+  specialEvents: router.get("/events/special", (req, res) => {
+    const specialEvents = require("../static_store/special_events");
+    if (specialEvents) {
+      return res.status(200).send(specialEvents);
+    }
+  }),
+};
 
 module.exports = router;
