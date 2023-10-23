d8ac4d72fe76602cdac9777063cc5f95ed35cf06
quantum-adrenaline-1 <yogiraj.chitam@gmail.com>
2023-10-23 13:27:10 +0530

    Formatted and validated linting rules for JS and MD for all the source-code and markdown files in the workspace using Prettier, ESLint and Markdownlint extensions respectively

--- jwt-authentication-backend-app/index.js
+++ jwt-authentication-backend-app/index.js
@@ -1,28 +1,28 @@
-const expressObj = require('express');
+const expressObj = require("express");
 
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
-const constants = require('./src/util/constant');
+const constants = require("./src/util/constant");
 
 // express application object
 const expressApplication = expressObj();
 
 // applying body parsing
-expressApplication.use(bodyParser.urlencoded({extended: false}));
+expressApplication.use(bodyParser.urlencoded({ extended: false }));
 expressApplication.use(bodyParser.json());
 
 // APIs on apiRouter
-const apiRouter = require('./src/api/');
+const apiRouter = require("./src/api/");
 
 // '/api' route assignment for APIs on 'apiRouter'
-expressApplication.use('/api', apiRouter);
+expressApplication.use("/api", apiRouter);
 
 // application health-status check
-expressApplication.get('/', (req, res) => {
-    res.send('Application "JWT authentication" is up and running');
+expressApplication.get("/", (req, res) => {
+  res.send('Application "JWT authentication" is up and running');
 });
 
 // start application on the assigned port
 expressApplication.listen(constants.PORT, (req, res) => {
-    console.log(`application is running on port ${constants.PORT}`);
+  console.log(`application is running on port ${constants.PORT}`);
 });
--- jwt-authentication-backend-app/src/persistence/index.js
+++ jwt-authentication-backend-app/src/persistence/index.js
@@ -1,13 +1,15 @@
-const mongooseObj = require('mongoose');
+const mongooseObj = require("mongoose");
 
-const connectionUrl = 'mongodb://localhost:27017/eventsdb';
+const connectionUrl = "mongodb://localhost:27017/eventsdb";
 
-const mongoDBConnectionObj = mongooseObj.connect(connectionUrl).then(connectionObj => connectionObj);
+const mongoDBConnectionObj = mongooseObj
+  .connect(connectionUrl)
+  .then((connectionObj) => connectionObj);
 
 if (mongoDBConnectionObj) {
-    console.log("Connected to MongoDB successfully!");
+  console.log("Connected to MongoDB successfully!");
 } else {
-    console.error("Connection to MongoDB failed!");
+  console.error("Connection to MongoDB failed!");
 }
 
 module.exports = mongoDBConnectionObj;
